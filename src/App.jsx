888bef418f05b5aa2ad0cd870d19b1c7c5804da0
Erik Hernandez <erikestuardo_161@hotmail.com>
2023-05-24 21:47:21 -0600

    Routing with React Router Dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
-
+import { useRoutes, BrowserRouter } from 'react-router-dom'
+import Home from './Pages/Home'
+import MyAccount from './Pages/MyAccount'
+import MyOrder from './Pages/MyOrder'
+import MyOrders from './Pages/MyOrders'
+import NotFound from './Pages/NotFound'
+import SignIn from './Pages/SignIn'
 import './App.css'
 
-function App() {
-  
 
+const AppRoutes = () => {
+
+  let routes = useRoutes([
+    {path:'/', element: <Home />},
+    {path:'/my-account', element: <MyAccount />},
+    {path:'/my-order', element: <MyOrder />},
+    {path:'/my-orders', element: <MyOrders />},
+    {path:'/*', element: <NotFound />},
+    {path:'/sign-in', element: <SignIn />},
+  ])
+
+  return routes
+}
+
+
+
+const App = () => {
+  
   return (
-    <>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!   
-    </h1>
-    </>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
