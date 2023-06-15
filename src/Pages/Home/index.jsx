122bf48ae14dd61b45b428208857f45a03c36a6d
Erik Hernandez <erikestuardo_161@hotmail.com>
2023-06-15 07:41:06 -0600

    Consuming the FakeStore API to paint cards

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -5,18 +5,24 @@ import Card from '../../Components/Card'
 function Home() {
   const [items, setItems] = useState(null)
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch('https://api.escuelajs.co/api/v1/products')
-    .then(response => response.json())
-    .then(data => console.log(data))
-  }, [] )
+      .then(response => response.json())
+      .then(data => setItems(data))
+  }, [])
 
   return (
     <Layout>
       Home
-      <Card />
+      <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+        {
+          items?.map(item => (
+            <Card key={item.id} data={item} />
+          ))
+        }
+      </div>
     </Layout>
   )
 }
 
-export default Home
+export default Home  
